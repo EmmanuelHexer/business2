25001177a56b19c85795beffd64e061fa27c72c3
EmmanuelHexer <Emmanuelhexer@gmail.com>
2025-03-22 10:52:39 +0000

    implementation of view more button

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -3,6 +3,11 @@ import { Element } from "react-scroll";
 
 const Products = () => {
   const [data, setData] = useState([]);
+  const [visibleCount, setVisibleCount] = useState(8);
+
+  const loadMore = () => {
+    setVisibleCount((prev) => prev + 8);
+  };
 
   useEffect(() => {
     async function getData() {
@@ -19,16 +24,23 @@ const Products = () => {
     <Element className="products no-padding-wrapper" name="products1">
       <h1>Food products</h1>
       <div className="foods">
-        {data.map(({ imageUrl, name, description, category }, index) => (
-          <div className="food" key={index}>
-            <img className="food-img" src={imageUrl} alt="" />
-            <div className="food-category">{category}</div>
-            <div className="food-name">{name}</div>
-            <div className="food-description">{description}</div>
-            <button className="food-btn">View Details</button>
-          </div>
-        ))}
+        {data
+          .slice(0, visibleCount)
+          .map(({ imageUrl, name, description, category }, index) => (
+            <div className="food" key={index}>
+              <img className="food-img" src={imageUrl} alt="" />
+              <div className="food-category">{category}</div>
+              <div className="food-name">{name}</div>
+              <div className="food-description">{description}</div>
+              <button className="food-btn">View Details</button>
+            </div>
+          ))}
       </div>
+      {visibleCount < data.length && (
+        <button onClick={() => loadMore()} className="load-food">
+          View More Food
+        </button>
+      )}
     </Element>
   );
 };
