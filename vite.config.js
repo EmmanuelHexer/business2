c4789c455303146fa7e357d9bb889e7f77f49c6b
EmmanuelHexer <Emmanuelhexer@gmail.com>
2025-03-30 23:56:02 +0000

    new changes

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import sitemap from "vite-plugin-sitemap";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     sitemap({
       hostname: "https://www.millyscuisine.com/",
-      generateRobotsTxt: false,
+      generateRobotsTxt: true,
+      robotsTxtOptions: {
+        policies: [{ userAgent: "*", allow: "/" }],
+      },
     }),
   ],
 });
