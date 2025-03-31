817b9a92b9d4eb908f1802836ff31d90eb389ec2
EmmanuelHexer <Emmanuelhexer@gmail.com>
2025-03-31 20:33:27 +0000

    new changes

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import sitemap from "vite-plugin-sitemap";
 
-// Your product data
 const products = [
-  { name: "Buffalo Wings", path: "/product/buffalo-wings" },
-  { name: "Club Sandwich", path: "/product/club-sandwich" },
+  { name: "Buffalo Wings", path: "/product/buffalo&20wings" },
+  { name: "Club Sandwich", path: "/product/club&20sandwich" },
   { name: "Cornbread", path: "/product/cornbread" },
-  { name: "Fried Chicken", path: "/product/fried-chicken" },
-  { name: "Tater Tots", path: "/product/tater-tots" },
-  { name: "Jollof Rice", path: "/product/jollof-rice" },
-  { name: "Ampesi with Garden Egg Stew", path: "/product/ampesi-garden-egg" },
-  { name: "Banku With Okro Stew", path: "/product/banku-okro" },
-  { name: "Ampesi with Palava Sauce", path: "/product/ampesi-palava" },
+  { name: "Fried Chicken", path: "/product/fried&20chicken" },
+  { name: "Tater Tots", path: "/product/tater&20tots" },
+  { name: "Jollof Rice", path: "/product/jollof&20rice" },
+  {
+    name: "Ampesi with Garden Egg Stew",
+    path: "/product/ampesi&20garden&20egg",
+  },
+  { name: "Banku With Okro Stew", path: "/product/banku&20okro" },
+  { name: "Ampesi with Palava Sauce", path: "/product/ampesi&20palava" },
 ];
 
 const dynamicRoutes = products.map((product) => product.path);
