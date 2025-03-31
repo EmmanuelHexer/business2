354e9e35aef5aa5b8787067e5b29758c8ed8c0be
EmmanuelHexer <Emmanuelhexer@gmail.com>
2025-03-31 20:27:27 +0000

    new changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import sitemap from "vite-plugin-sitemap";
+
+// Your product data
+const products = [
+  { name: "Buffalo Wings", path: "/product/buffalo-wings" },
+  { name: "Club Sandwich", path: "/product/club-sandwich" },
+  { name: "Cornbread", path: "/product/cornbread" },
+  { name: "Fried Chicken", path: "/product/fried-chicken" },
+  { name: "Tater Tots", path: "/product/tater-tots" },
+  { name: "Jollof Rice", path: "/product/jollof-rice" },
+  { name: "Ampesi with Garden Egg Stew", path: "/product/ampesi-garden-egg" },
+  { name: "Banku With Okro Stew", path: "/product/banku-okro" },
+  { name: "Ampesi with Palava Sauce", path: "/product/ampesi-palava" },
+];
+
+const dynamicRoutes = products.map((product) => product.path);
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: "https://www.millyscuisine.com/",
+      dynamicRoutes,
+    }),
+  ],
 });
