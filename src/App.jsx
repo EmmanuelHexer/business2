32de61241cbf054559b0abbe95d605f1ae2c210b
EmmanuelHexer <Emmanuelhexer@gmail.com>
2025-04-03 12:28:45 +0000

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import Home from "./Pages/Home";
 import ProductsPage from "./Pages/ProductsPage";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import ScrollDefault from "./Components/ScrollDefault";
+import NotFoundPage from "./Components/NotFoundPage";
 
 const App = () => {
-  const NotFound = () => <h1>404 - Page Not Found</h1>;
   return (
     <div>
       <ScrollDefault />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product/:name" element={<ProductsPage />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <SpeedInsights />
     </div>
---
+++ src/Components/NotFoundPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div className="page-not-found">
+      <div className="page-not-found-container">
+        <h1>404 - Page Not Found</h1>
+        <a href="https://millyscuisine.com/">Back To Home</a>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
