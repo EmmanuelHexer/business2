6a07ad41506419cdc7bab09b9031bde69bb2656f
EmmanuelHexer <Emmanuelhexer@gmail.com>
2025-04-03 11:56:45 +0000

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import { SpeedInsights } from "@vercel/speed-insights/react";
 import ScrollDefault from "./Components/ScrollDefault";
 
 const App = () => {
+  const NotFound = () => <h1>404 - Page Not Found</h1>;
   return (
     <div>
       <ScrollDefault />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product/:name" element={<ProductsPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <SpeedInsights />
     </div>
