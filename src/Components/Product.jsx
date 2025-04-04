ea150bcc698195470acd7921ed27d8f77effa4c7
EmmanuelHexer <Emmanuelhexer@gmail.com>
2025-04-04 03:38:36 +0000

    add alt attribute names

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Product = ({ imageUrl, category, foodName, description }) => {
   return (
     <div className="food">
-      <img className="food-img" src={imageUrl} alt="" />
+      <img className="food-img" src={imageUrl} alt={foodName} />
       <div className="food-category">{category}</div>
       <div className="food-name">{foodName}</div>
       <div className="food-description">{description}</div>
